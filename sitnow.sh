04adc6081cb4c14b1e9d677bc5ab7444919b409a
Remo Tomasi <remo.tomasi@gmail.com>
2016-03-03 23:33:22 +0100

    Update and rename prev.sh to sitnow.sh

--- sitnow.sh
+++ sitnow.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Remember to insert the appid parameter obtened on openweathermap.org
+appid=
 clear
 echo "Insert weather location >>"
 read paese
 clear
-wget -O w1.xml "http://api.openweathermap.org/data/2.5/forecast?q=$paese,it&mode=xml" 2>/dev/null
+wget -O w1.xml "http://api.openweathermap.org/data/2.5/forecast?q=$paese&mode=xml&appid=$appid" 2>/dev/null
 wget -O w2.xml "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$paese%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys" 2>/dev/null
 
 pr1=`cat w1.xml | grep precipitation | head -1 | cut -d'"' -f2`
@@ -19,7 +21,7 @@ c1=`cat w1.xml | tr '<' '\n' | grep "clouds value" | head -1 | cut -d'"' -f4`
 
 # echo -e `echo "scale=2; $pr1" | bc -l` 'mm'
 # echo -e $dir1 '\t' `echo "scale=2; $s1" | bc -l` 'kph'
-# echo -e $t11 "°C" '\t' $t12 "°C" 
+# echo -e $t11 "°C" '\t' $t12 "°C"
 # echo -e `echo $p1 | bc -l` "hPa"
 # echo -e $h1 "%"
 # echo -e $c1 "%"
@@ -49,7 +51,7 @@ c2=`cat w2.xml | grep BR | head -1 | cut -d',' -f1`
 # echo -e `echo "scale=2; $pr1" | bc -l` 'mm'
 
 # echo -e $dir2 # '\t' `echo "scale=2; $s" | bc -l` 'kph'
-# echo -e `echo "scale=2; $t21" | bc -l` "°C" '\t' `echo "scale=2; $t22" | bc -l` "°C" 
+# echo -e `echo "scale=2; $t21" | bc -l` "°C" '\t' `echo "scale=2; $t22" | bc -l` "°C"
 p2=$p2*33.8
 # echo -e `echo $p2 | bc -l` "hPa"
 # echo -e $h2 "%"
@@ -65,4 +67,4 @@ echo -e "Temperature: " '\t\t\t'  `echo "scale=2; ($t11+$t21)/2" | bc -l` "°C" 
 echo -e "Pressione: " '\t\t\t' `echo "scale=2; ($p1+$p2)/2" | bc -l` "hPa"
 echo -e "Umidita': " '\t\t\t' `echo "scale=2; ($h1+$h2)/2" | bc -l` "%"
 echo -e "Copertura nuvolosa/fenomeni: " '\t' $c1 "%" "/" $c2
-echo 
+echo
